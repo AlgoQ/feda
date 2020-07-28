b0a9452d649b148da6462c97b225d3bd74cb5c5c
JanssensKobe <kobejanssens@outlook.com>
2020-07-28 13:27:00 +0200

    added filename to write to

--- histCryptoDatafeed.py
+++ histCryptoDatafeed.py
@@ -4,7 +4,8 @@ import calendar
 # Extern modules
 import ccxt
 class histCryptoDatafeed:
-    def __init__(self, pair:str, days:int, strExchange:str='binanceFutures', interval:str='1m'):
+    def __init__(self, fileName:str, pair:str, days:int, strExchange:str='binanceFutures', interval:str='1m'):
+        self.fileName = fileName
         self.pair = pair
         self.days = days
         self.strExchange = strExchange
@@ -30,55 +31,58 @@ class histCryptoDatafeed:
         else:
             raise Exception('Exchange is not supported')
 
-    def _calcMillis(self, interval, limit):
+    def _calcMillis(self):
         minute = 60000
-        if interval == '1m':
-            return minute * limit
-        elif interval == '3m':
-            return minute * 3 * limit
-        elif interval == '5m':
-            return minute * 5 * limit
-        elif interval == '15m':
-            return minute * 15 * limit
-        elif interval == '30m':
-            return minute * 30 * limit
-        elif interval == '45m':
-            return minute * 45 * limit
-        elif interval == '1h':
-            return minute * 60 * limit
-        elif interval == '2h':
-            return minute * 60 * 2 * limit
-        elif interval == '4h':
-            return minute * 60 * 4 * limit
-        elif interval == '1d':
-            return minute * 60 * 24 * limit
-        elif interval == '1w':
-            return minute * 60 * 24 * 7 * limit
-        elif interval == '1w':
-            return minute * 60 * 24 * 30 * limit
+
+        if self.interval == '1m':
+            return minute * self.limit
+        elif self.interval == '3m':
+            return minute * 3 * self.limit
+        elif self.interval == '5m':
+            return minute * 5 * self.limit
+        elif self.interval == '15m':
+            return minute * 15 * self.limit
+        elif self.interval == '30m':
+            return minute * 30 * self.limit
+        elif self.interval == '45m':
+            return minute * 45 * self.limit
+        elif self.interval == '1h':
+            return minute * 60 * self.limit
+        elif self.interval == '2h':
+            return minute * 60 * 2 * self.limit
+        elif self.interval == '4h':
+            return minute * 60 * 4 * self.limit
+        elif self.interval == '1d':
+            return minute * 60 * 24 * self.limit
+        elif self.interval == '1w':
+            return minute * 60 * 24 * 7 * self.limit
+        elif self.interval == '1M':
+            return minute * 60 * 24 * 30 * self.limit
         else:
             raise Exception('Unsupported interval')
 
 
-    def histCryptoDatafeed(self, pair:str, days:int, exchange='binanceFutures', interval='1m'):
-        startSince = exchange.milliseconds() - 86400000 * days
-        until = datetime.now().strftime("%d/%m/%Y %H:%M:%S").timestamp() * 1000
-
-        diffBtwnUntilSince = int((until - startSince).total_seconds() / 60)
-        loopCount = diffBtwnUntilSince / self.limit
+    def fetchDatafeed(self):
+        startSince = self.exchange.milliseconds() - 86400000 * self.days
+        until = round(datetime.now().timestamp() * 1000)
+        diffBtwnUntilSince = round(int(until - startSince)/(1000*60))
+        loopCount = int(diffBtwnUntilSince / self.limit)
 
         fullOhlcv = []
-
-        fullOhlcv.append(exchange.fetch_ohlcv(symbol=pair, limit=self.limit, timeframe=interval, since=startSince))
-        millisCycle = self._calcMillis(interval, self.limit)
-
+        
+        fullOhlcv = self.exchange.fetch_ohlcv(symbol=self.pair, limit=self.limit, timeframe=self.interval, since=startSince)
+        millisCycle = self._calcMillis()
+        
         since = startSince + millisCycle
         for i in range(loopCount - 1):
-            fullOhlcv.append(exchange.fetch_ohlcv(symbol=pair, limit=self.limit, timeframe=interval, since=since))
+            fullOhlcv = fullOhlcv + self.exchange.fetch_ohlcv(symbol=self.pair, limit=self.limit, timeframe=self.interval, since=since)
             since += millisCycle
 
-        # TODO: If item is already in the list don't add it
-        # TODO: Check release of a specific market pair
-        
+        # # TODO: If item is already in the list don't add it
 
-    # histCryptoDatafeed(pair='BTC/USDT', days=60, exchange='binanceFutures', interval='1m')
+        f = open(self.fileName, "w")
+
+        f.write(str(fullOhlcv))
+        f.close()
+
+        return fullOhlcv
